0ab9795c92fc7ced656112b4e27b3b4763bb5ad9
EvgeniiMal <eugenemal88@gmail.com>
2020-03-04 15:20:57 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 != 0) {
+        return false;
+    }
+
+    let open = [];
+    let close = [];
+    let ident = [];
+    let stack = [];
+    let strArr = str.split('');
+
+    bracketsConfig.forEach(element => {
+        if (element[0] !== element[1]) {
+            open.push(element[0]);
+            close.push(element[1]);
+        } else {
+            ident.push(element[0]);
+        }
+    });
+
+    for (let i = 0; i < strArr.length; i++) {
+        let element = strArr[i];
+
+        if (ident.indexOf(element) != -1) {
+            if (stack[stack.length - 1] !== element) {
+                stack.push(element)
+            } else {
+                stack.pop();
+            }
+        } else if (open.indexOf(element) != -1) {
+            stack.push(close[open.indexOf(element)]);
+        } else {
+            let nextClose = stack.pop();
+            if (nextClose !== element) {
+                return false;
+            }
+        }
+    }
+    return stack.length == 0
 }
+
